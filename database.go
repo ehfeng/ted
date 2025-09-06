807e10e5025113f74d5f931b99c6e9a8b8ff4f92
Eric Feng <erichfeng@gmail.com>
2025-09-06 09:53:32 -0700

    table lookup key
    will be needed for updates

--- database.go
+++ database.go
@@ -7,9 +7,10 @@ import (
 )
 
 type Table struct {
-	Name    string
-	Columns []Column
-	Rows    [][]interface{}
+	Name      string
+	LookupKey []string // empty if no primary key
+	Columns   []Column
+	Rows      [][]interface{}
 }
 
 type Column struct {
@@ -43,7 +44,7 @@ func parseTableSpec(tableSpec string) (string, []string) {
 
 func loadTable(db *sql.DB, dbType DatabaseType, tableSpec string) (*Table, error) {
 	tableName, columns := parseTableSpec(tableSpec)
-	
+
 	if tableName == "" {
 		return nil, fmt.Errorf("table name is required")
 	}
@@ -65,7 +66,7 @@ func loadTable(db *sql.DB, dbType DatabaseType, tableSpec string) (*Table, error
 
 func loadTableSchema(db *sql.DB, dbType DatabaseType, table *Table) error {
 	var query string
-	
+
 	switch dbType {
 	case SQLite:
 		query = fmt.Sprintf("PRAGMA table_info(%s)", table.Name)
@@ -87,22 +88,27 @@ func loadTableSchema(db *sql.DB, dbType DatabaseType, table *Table) error {
 	}
 	defer rows.Close()
 
+	var primaryKeyColumns []string
+
 	for rows.Next() {
 		var col Column
 		var nullable string
-		
+
 		switch dbType {
 		case SQLite:
 			var cid int
 			var dfltValue sql.NullString
 			var pk int
-			err = rows.Scan(&cid, &col.Name, &col.Type, &pk, &dfltValue, &nullable)
+			err = rows.Scan(&cid, &col.Name, &col.Type, &nullable, &dfltValue, &pk)
 			col.Nullable = nullable != "1"
+			if pk == 1 {
+				primaryKeyColumns = append(primaryKeyColumns, col.Name)
+			}
 		case PostgreSQL, MySQL:
 			err = rows.Scan(&col.Name, &col.Type, &nullable)
 			col.Nullable = strings.ToLower(nullable) == "yes"
 		}
-		
+
 		if err != nil {
 			return err
 		}
@@ -111,12 +117,35 @@ func loadTableSchema(db *sql.DB, dbType DatabaseType, table *Table) error {
 		table.Columns = append(table.Columns, col)
 	}
 
-	return rows.Err()
+	if err := rows.Err(); err != nil {
+		return err
+	}
+
+	// For PostgreSQL and MySQL, we need separate queries to get primary key info
+	if dbType == PostgreSQL || dbType == MySQL {
+		pkCols, err := getPrimaryKeyColumns(db, dbType, table.Name)
+		if err != nil {
+			return err
+		}
+		primaryKeyColumns = pkCols
+	}
+
+	// If no primary key found, try to find the simplest unique constraint
+	if len(primaryKeyColumns) == 0 {
+		uniqueCols, err := getSimplestUniqueConstraint(db, dbType, table.Name)
+		if err != nil {
+			return err
+		}
+		primaryKeyColumns = uniqueCols
+	}
+
+	table.LookupKey = primaryKeyColumns
+	return nil
 }
 
 func loadTableData(db *sql.DB, table *Table, requestedColumns []string) error {
 	var columnNames []string
-	
+
 	if len(requestedColumns) > 0 {
 		columnNames = requestedColumns
 		filteredCols := make([]Column, 0)
@@ -136,7 +165,7 @@ func loadTableData(db *sql.DB, table *Table, requestedColumns []string) error {
 	}
 
 	query := fmt.Sprintf("SELECT %s FROM %s LIMIT 1000", strings.Join(columnNames, ", "), table.Name)
-	
+
 	rows, err := db.Query(query)
 	if err != nil {
 		return err
@@ -167,6 +196,167 @@ func loadTableData(db *sql.DB, table *Table, requestedColumns []string) error {
 	}
 
 	return rows.Err()
+}
+
+func getPrimaryKeyColumns(db *sql.DB, dbType DatabaseType, tableName string) ([]string, error) {
+	var query string
+	var columns []string
+
+	switch dbType {
+	case PostgreSQL:
+		query = `SELECT a.attname
+				FROM pg_index i
+				JOIN pg_attribute a ON a.attrelid = i.indrelid AND a.attnum = ANY(i.indkey)
+				WHERE i.indrelid = $1::regclass AND i.indisprimary
+				ORDER BY a.attnum`
+	case MySQL:
+		query = `SELECT column_name
+				FROM information_schema.key_column_usage
+				WHERE table_name = ? AND constraint_name = 'PRIMARY'
+				ORDER BY ordinal_position`
+	default:
+		return columns, nil
+	}
+
+	rows, err := db.Query(query, tableName)
+	if err != nil {
+		return columns, err
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var colName string
+		if err := rows.Scan(&colName); err != nil {
+			return columns, err
+		}
+		columns = append(columns, colName)
+	}
+
+	return columns, rows.Err()
+}
+
+func getSimplestUniqueConstraint(db *sql.DB, dbType DatabaseType, tableName string) ([]string, error) {
+	var query string
+	var columns []string
+
+	switch dbType {
+	case SQLite:
+		query = fmt.Sprintf("PRAGMA index_list(%s)", tableName)
+	case PostgreSQL:
+		query = `SELECT i.indexname, array_agg(a.attname ORDER BY a.attnum) as columns
+				FROM pg_index idx
+				JOIN pg_class c ON c.oid = idx.indrelid
+				JOIN pg_class i ON i.oid = idx.indexrelid
+				JOIN pg_attribute a ON a.attrelid = idx.indrelid AND a.attnum = ANY(idx.indkey)
+				WHERE c.relname = $1 AND idx.indisunique AND NOT idx.indisprimary
+				GROUP BY i.indexname, array_length(idx.indkey, 1)
+				ORDER BY array_length(idx.indkey, 1), i.indexname
+				LIMIT 1`
+	case MySQL:
+		query = `SELECT column_name
+				FROM information_schema.statistics
+				WHERE table_name = ? AND non_unique = 0 AND index_name != 'PRIMARY'
+				GROUP BY index_name
+				ORDER BY COUNT(*), index_name
+				LIMIT 1`
+	default:
+		return columns, nil
+	}
+
+	switch dbType {
+	case SQLite:
+		rows, err := db.Query(query)
+		if err != nil {
+			return columns, err
+		}
+		defer rows.Close()
+
+		var bestIndex string
+		minColumns := 999
+
+		for rows.Next() {
+			var seq int
+			var name string
+			var unique int
+			var origin string
+			var partial int
+			if err := rows.Scan(&seq, &name, &unique, &origin, &partial); err != nil {
+				continue
+			}
+
+			if unique == 1 {
+				indexInfo, err := db.Query(fmt.Sprintf("PRAGMA index_info(%s)", name))
+				if err != nil {
+					continue
+				}
+				defer indexInfo.Close()
+
+				var colCount int
+				for indexInfo.Next() {
+					colCount++
+				}
+
+				if colCount < minColumns {
+					minColumns = colCount
+					bestIndex = name
+				}
+			}
+		}
+
+		if bestIndex != "" {
+			indexInfo, err := db.Query(fmt.Sprintf("PRAGMA index_info(%s)", bestIndex))
+			if err != nil {
+				return columns, err
+			}
+			defer indexInfo.Close()
+
+			for indexInfo.Next() {
+				var seqno, cid int
+				var name string
+				if err := indexInfo.Scan(&seqno, &cid, &name); err != nil {
+					continue
+				}
+				columns = append(columns, name)
+			}
+		}
+
+	case PostgreSQL:
+		rows, err := db.Query(query, tableName)
+		if err != nil {
+			return columns, err
+		}
+		defer rows.Close()
+
+		if rows.Next() {
+			var indexName string
+			var colArray string
+			if err := rows.Scan(&indexName, &colArray); err != nil {
+				return columns, err
+			}
+			// Parse PostgreSQL array format {col1,col2}
+			colArray = strings.Trim(colArray, "{}")
+			if colArray != "" {
+				columns = strings.Split(colArray, ",")
+			}
+		}
+
+	case MySQL:
+		rows, err := db.Query(query, tableName)
+		if err != nil {
+			return columns, err
+		}
+		defer rows.Close()
+
+		for rows.Next() {
+			var colName string
+			if err := rows.Scan(&colName); err != nil {
+				continue
+			}
+			columns = append(columns, colName)
+		}
+	}
+
+	return columns, nil
 }
 
 func max(a, b int) int {
